2d09a05e292b60d0afaaf55b73b3b9aef0ed77cd
LimeFallacie\Emil <e0014667@u.nus.edu>
2019-10-20 14:25:49 +0800

    huff puff added display components

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+  render() {
+    return(
+      <nav className="navbar navbar-dark text-white bg-dark">
+        <span className="navbar-brand mb-0 h1">React+API</span>
+        
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
